dfe213e2dbca1787bf353d4d3906f00afc45247b
Nick Bruinsma <nickbruinsma@gmail.com>
2018-01-14 21:28:25 +0100

    added bug-detail and model

--- app/bugs/bug-list/bug-list.component.ts
+++ app/bugs/bug-list/bug-list.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { BugService } from '../service/bug.service';
 
+import { Bug } from '../model/bug';
 @Component({
     moduleId: module.id,
     selector: 'bug-list',
     templateUrl: 'bug-list.component.html',
-    styleUrls: [ 'bug-list.component.css' ]
+    styleUrls: ['bug-list.component.css']
 })
-export class BugListComponent { 
+export class BugListComponent implements OnInit {
 
-    constructor(private bugService: BugServ)
+    private bugs: Bug[] = [];
+
+    constructor(private bugService: BugService) { }
+
+    ngOnInit() {
+        this.getAddedBugs();
+    }
+
+    getAddedBugs() {
+        this.bugService.getAddedBugs()
+            .subscribe(bug => {
+                this.bugs.push(bug);
+                console.log(this.bugs); // TODO: REMOVE
+            },
+            err => {
+                console.error("Unable to get added bug - ", err);
+            });
+    }
 }
--- app/bugs/bug.module.ts
+++ app/bugs/bug.module.ts
@@ -5,6 +5,10 @@ import { BugRoutingModule } from './bug-routing.module';
 
 // Component
 import { BugListComponent } from './bug-list/bug-list.component';
+import { BugDetailComponent } from './bug-detail/bug-detail.component';
+
+// Service
+import { BugService } from './service/bug.service';
 
 @NgModule({
     imports: [
@@ -13,10 +17,11 @@ import { BugListComponent } from './bug-list/bug-list.component';
     ],
     declarations: [
         BugListComponent,
+        BugDetailComponent
     ],
     exports: [],
     providers: [
-        
+        BugService
     ]
 })
 export class BugModule { }
